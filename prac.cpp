754349ac55b566eba358076b79c4f9fc1579731e
박민석 <112817284+RockThePeople@users.noreply.github.com>
2023-10-01 16:57:50 +0000

    on Ipad

---
+++ prac.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    vector<int> a;
+    a.push_back(1);
+    printf("%d", a[0]);
+    cout << a[0];
+    return 0;
+}
